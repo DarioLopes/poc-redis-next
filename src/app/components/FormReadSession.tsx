157348b9c5ef0d50347c8b71b203dfe168be4a05
Dario Lopes <dariolopes4v@gmail.com>
2026-01-01 16:31:04 +0100

    fix: update session without error

--- src/app/components/FormReadSession.tsx
+++ src/app/components/FormReadSession.tsx
@@ -15,27 +15,8 @@ export const FormReadSession = () => {
 	return (
 		<form action={action} className="flex flex-col items-center gap-3 w-full max-w-xs lg:min-w-xs border border-gray-800 rounded-md p-4">
 			<button type="submit" disabled={isPending} className="bg-gray-800 mb-2 text-white px-4 py-2 rounded disabled:opacity-60 w-full" onClick={() => setHasClicked(true)}>
-				{isPending ? 'Loading...' : 'Read session'}
+				{isPending ? 'Loading...' : 'Read session from server'}
 			</button>
-
-			{session.status === 'idle' && <p className="text-gray-500">Loading...</p>}
-
-			{session.status === 'empty' && <p className="text-gray-500">No session data found.</p>}
-
-			{session.status === 'success' && (
-				<div className="mt-2 rounded-lg border border-gray-800 p-4 w-full max-w-sm">
-					<p className="font-semibold mb-2">Session values</p>
-
-					<ul className="text-sm space-y-1">
-						{Object.entries(session.session).map(([key, value]) => (
-							<li key={key}>
-								<span className="font-mono text-gray-600">{key}: </span>
-								<span className="font-semibold">{typeof value === 'string' ? value : JSON.stringify(value)}</span>
-							</li>
-						))}
-					</ul>
-				</div>
-			)}
 
 			{hasClicked && session.status !== 'empty' && state.status === 'success' && (
 				<div className="rounded-lg border border-gray-800 p-4 w-full max-w-sm">
@@ -49,6 +30,25 @@ export const FormReadSession = () => {
 
 					<ul className="text-sm space-y-1">
 						{Object.entries(state.session).map(([key, value]) => (
+							<li key={key}>
+								<span className="font-mono text-gray-600">{key}: </span>
+								<span className="font-semibold">{typeof value === 'string' ? value : JSON.stringify(value)}</span>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+
+			{session.status === 'idle' && <p className="text-gray-500">Loading...</p>}
+
+			{session.status === 'empty' && <p className="text-gray-500">No session data found.</p>}
+
+			{session.status === 'success' && (
+				<div className="mt-2 rounded-lg border border-gray-800 p-4 w-full max-w-sm">
+					<p className="font-semibold mb-2">Session values</p>
+
+					<ul className="text-sm space-y-1">
+						{Object.entries(session.session).map(([key, value]) => (
 							<li key={key}>
 								<span className="font-mono text-gray-600">{key}: </span>
 								<span className="font-semibold">{typeof value === 'string' ? value : JSON.stringify(value)}</span>
